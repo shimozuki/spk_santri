463e4d2a61b0113c2131f8f72b52252d5251c4bf
robbishimozuki <robbishimozuki@gmail.com>
2025-05-21 15:50:12 +0800

    firs

--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -3,32 +3,30 @@
 @push('style')
 @endpush
 @section('main')
-    <div class="mb-4">
+<div class="mb-4">
 
-        <div class="row">
-            @if (Auth::user()->role != 'User')
-                <div class="container d-flex justify-content-center align-self-center p-5">
-                    <h3 class="font-weight-bold text-center">Selamat Datang {{ Auth::user()->name }} Di Aplikasi PILMAPRES
-                        IIB Darmajaya
-                        Menggunakan
-                        Metode Profile Matching & Bubble Sort
-                    </h3>
-                </div>
-            @else
-                <div class="container d-flex justify-content-center align-self-center p-5">
-                    <h4 class="font-weight-bold text-center">Selamat Datang {{ Auth::user()->name }} Di Aplikasi PILMAPRES
-                        IIB Darmajaya
-                        <br>
-                        @if (empty(Auth::user()->alternatif->semester1))
-                            Harap Segera lengkapi persyaratan di halaman <a href="{{ route('profile.index') }}">profile</a>
-                        @else
-                            Untuk informasi lebih lanjut silahkan kunjungi instagram <a href="">KMH Darmajaya</a>
-                        @endif
-                    </h4>
-                </div>
-            @endif
+    <div class="row">
+        @if (Auth::user()->role != 'User')
+        <div class="container d-flex justify-content-center align-self-center p-5">
+            <h3 class="font-weight-bold text-center">Selamat Datang {{ Auth::user()->name }} Di Aplikasi Sistem Pendukung Keputusan Lulusan Santri
+                Menggunakan
+                Metode Profile Matching
+            </h3>
         </div>
+        @else
+        <div class="container d-flex justify-content-center align-self-center p-5">
+            <h4 class="font-weight-bold text-center">Selamat Datang {{ Auth::user()->name }} Di Aplikasi Sistem Pendukung Keputusan Lulusan Santri
+                <br>
+                @if (empty(Auth::user()->alternatif->semester1))
+                Harap Segera lengkapi persyaratan di halaman <a href="{{ route('profile.index') }}">profile</a>
+                @else
+                Untuk informasi lebih lanjut silahkan kunjungi instagram <a href="">KMH Darmajaya</a>
+                @endif
+            </h4>
+        </div>
+        @endif
     </div>
+</div>
 @endsection
 @push('script')
 @endpush
